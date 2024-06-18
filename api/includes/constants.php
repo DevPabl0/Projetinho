4973a53d563329c7f547dc355c322d191124b976
ArthurGSO <arthurprobr422@gmail.com>
2024-06-17 22:14:56 -0300

    login api - vscode

--- api/includes/constants.php
+++ api/includes/constants.php
@@ -1,7 +1,6 @@
 <?php
-
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'arapa');
-    define('DB_PASS', 'rpa');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
     define('DB_NAME', 'BikesDB');
 ?>
